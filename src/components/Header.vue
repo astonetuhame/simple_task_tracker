f80e2528effe3571964fce820161ad9de1e4eeac
Tuhame Astone <astonetuhame@gmail.com>
2021-12-13 13:09:44 +0300

    Created Header

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button text="Add Task" color="green" />
+  </header>
+</template>
+
+
+<script>
+import Button from './Button';
+
+
+export default {
+  name: "Header",
+  components: {
+    Button
+  },
+  props : {
+      title: String
+  }
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
