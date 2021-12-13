4b7db865d6c3a6d1dbcc1baf1580055bb3e00554
Tuhame Astone <astonetuhame@gmail.com>
2021-12-13 14:37:30 +0300

    Updated tasks component

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,15 +1,22 @@
 <template>
     <div v-for="(task, index) in tasks" :key="index">
-        <h3>{{ task.text}}</h3>
+        <Task @toggle-reminder="$emit('toggle-reminder', task.id)" @delete-task="$emit('delete-task', task.id)" :task="task" />
     </div>
 </template>
 
 
 <script>
+
+import Task from './Task'
+
 export default {
     name: "Tasks",
     props: {
         tasks: Array
-    }
+    },
+    components: {
+    Task
+  },
+   emits: ['delete-task', 'toggle-reminder'],
 }
 </script>
