153680ef26234bef75c85939bf4ed7b0387adfd0
Tuhame Astone <astonetuhame@gmail.com>
2021-12-13 14:38:37 +0300

    Updated app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,76 @@
 <template>
-<div class="container">
-  <Header  title="Task Tracker" />
-  <Tasks :tasks="tasks"/>
-</div>
+  <div class="container">
+    <Header @toggle-add-task="toggleAddTask" title="Task Tracker" :showAddTask="showAddTask" />
+    <div>
+      <AddTask v-show="showAddTask" @add-task="addTask" />
+    </div>
+    <Tasks
+      @toggle-reminder="toggleReminder"
+      @delete-task="deleteTask"
+      :tasks="tasks"
+    />
+  </div>
 </template>
 
 <script>
-
-import Header from './components/Header';
-import Tasks from './components/Tasks';
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 export default {
   name: "App",
   components: {
     Header,
-    Tasks
+    Tasks,
+    AddTask,
   },
-  data(){
-    return{
-      tasks: []
+  data() {
+    return {
+      tasks: [],
+      showAddTask: false,
+    };
+  },
+  methods: {
+    addTask(task) {
+      this.tasks = [...this.tasks, task];
+    },
+
+    deleteTask(id) {
+      if (confirm("Are you sure?"))
+        this.tasks = this.tasks.filter((task) => task.id !== id);
+    },
+    toggleReminder(id) {
+      this.tasks = this.tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      );
+    },
+    toggleAddTask()
+    {
+      this.showAddTask = !this.showAddTask
     }
   },
-  created(){
+  created() {
     this.tasks = [
       {
         id: 1,
-        text: 'Doctors Appointment',
-        day: 'March 2nd 1t 1:30pm',
+        text: "Doctors Appointment",
+        day: "March 2nd At 1:30pm",
         reminder: true,
-      }
-    ]
-  }
+      },
+      {
+        id: 2,
+        text: "Business meeting",
+        day: "April 24th At 4:30pm",
+        reminder: false,
+      },
+      {
+        id: 3,
+        text: "Date with wife",
+        day: "May 14th At 8:30pm",
+        reminder: true,
+      },
+    ];
+  },
 };
 </script>
 
