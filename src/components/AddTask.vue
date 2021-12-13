31f629a197d3256a9fce7e099579679d7a6d66c0
Tuhame Astone <astonetuhame@gmail.com>
2021-12-13 16:50:41 +0300

    Updated addtask component

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -42,7 +42,7 @@ export default {
             }
 
             const newTask = {
-                id: Math.floor(Math.random() *1000),
+                // id: Math.floor(Math.random() *1000),
                 text: this.text,
                 day: this.day,
                 reminder: this.reminder
