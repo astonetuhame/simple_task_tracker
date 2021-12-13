f4be5e899e34a6aa9f715172779dab25c55651d6
Tuhame Astone <astonetuhame@gmail.com>
2021-12-13 13:11:00 +0300

    Created Tasks component

---
+++ src/components/Tasks.vue
@@ -0,0 +1,15 @@
+<template>
+    <div v-for="(task, index) in tasks" :key="index">
+        <h3>{{ task.text}}</h3>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "Tasks",
+    props: {
+        tasks: Array
+    }
+}
+</script>
